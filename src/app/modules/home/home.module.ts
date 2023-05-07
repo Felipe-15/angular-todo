0544fea0d17d405f8fdfe9b0e8ba445a29a1441d
Felipe Souza <felipesouza036@gmail.com>
2023-05-07 12:30:30 -0300

    Adding Forms Module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { HomeComponent } from './pages/home/home.component';
-
-
 
 @NgModule({
   declarations: [
@@ -14,10 +13,8 @@ import { HomeComponent } from './pages/home/home.component';
     TodoButtonDeleteAllComponent,
     TodoInputAddItemsComponent,
     TodoListComponent,
-    HomeComponent
+    HomeComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FormsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
