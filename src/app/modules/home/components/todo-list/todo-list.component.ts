f388a6e6c5280680da324832984d3d440705cc5c
Felipe Souza <felipesouza036@gmail.com>
2023-05-07 12:46:20 -0300

    Todo List basic logic

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../model/task';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.sass']
+  styleUrls: ['./todo-list.component.sass'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  taskList: Task[] = [{ checked: true, text: 'Primeira task' }];
 
+  deleteTask(taskIndex: number): void {
+    this.taskList.splice(taskIndex, 1);
+  }
+
+  deleteAll(): void {
+    const confirm = window.confirm(
+      'Are you sure you want to delete all tasks?'
+    );
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+
+  ngOnInit(): void {}
 }
